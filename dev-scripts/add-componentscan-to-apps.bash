b2efcca49e12371b1a20a292aa41353ee7b386a0
iannagasen <iann.agasen@gmail.com>
2023-11-14 08:18:59 +0800

    Add Script to automate addition of @ComponentScan to projects

---
+++ dev-scripts/add-componentscan-to-apps.bash
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+echo $(pwd)
+cd ..
+WORKING_DIR=$(pwd)/microservices
+
+## Add @ComponentScan annotation for each microservice
+find "$WORKING_DIR" -type f -name '*Application.java' -print0 \
+| while IFS= read -r -d '' JAVA_APP_FILE
+do 
+  echo "$JAVA_APP_FILE"
+  if ! grep -q '@ComponentScan("dev.agasen")' "$JAVA_APP_FILE"
+  then
+    sed -i '/import org.springframework.boot.SpringApplication;/ a\import org.springframework.context.annotation.ComponentScan;' "$JAVA_APP_FILE"
+    sed -i '/@SpringBootApplication/ i\@ComponentScan("dev.agasen")' "$JAVA_APP_FILE"
+    cat "$JAVA_APP_FILE"
+  else
+    echo "ComponentScan already added"
+  fi 
+  sleep 2
+done
