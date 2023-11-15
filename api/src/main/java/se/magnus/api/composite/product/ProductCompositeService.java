16a455d6b664ac4e4f8ec4b167bf008b924e4c02
iannagasen <iann.agasen@gmail.com>
2023-11-16 02:25:13 +0800

    Add OpenAPI Documentation

--- api/src/main/java/se/magnus/api/composite/product/ProductCompositeService.java
+++ api/src/main/java/se/magnus/api/composite/product/ProductCompositeService.java
@@ -3,6 +3,10 @@ package se.magnus.api.composite.product;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 public interface ProductCompositeService {
 
   /**
@@ -11,6 +15,21 @@ public interface ProductCompositeService {
    * @param productId Id of the product
    * @return the composite product info, if found, else null
    */
+    /**
+   * Sample usage: "curl $HOST:$PORT/product-composite/1".
+   *
+   * @param productId Id of the product
+   * @return the composite product info, if found, else null
+   */
+  @Operation(
+    summary = "${api.product-composite.get-composite-product.description}",
+    description = "${api.product-composite.get-composite-product.notes}")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "${api.responseCodes.ok.description}"),
+    @ApiResponse(responseCode = "400", description = "${api.responseCodes.badRequest.description}"),
+    @ApiResponse(responseCode = "404", description = "${api.responseCodes.notFound.description}"),
+    @ApiResponse(responseCode = "422", description = "${api.responseCodes.unprocessableEntity.description}")
+  })
   @GetMapping(
     value = "/product-composite/{productId}",
     produces = "application/json")
