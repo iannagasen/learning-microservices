88d97b6ff1f7ceaeb0aead7e45dc4d6d231d344e
iannagasen <iann.agasen@gmail.com>
2023-11-17 13:04:13 +0800

    Add review-service Persistence Tests

--- microservices/review-service/src/main/java/dev/agasen/microsrv/core/review/persistence/ReviewEntity.java
+++ microservices/review-service/src/main/java/dev/agasen/microsrv/core/review/persistence/ReviewEntity.java
@@ -28,4 +28,12 @@ public class ReviewEntity {
   private String author;
   private String subject;
   private String content;
+  
+  public ReviewEntity(int productId, int reviewId, String author, String subject, String content) {
+    this.productId = productId;
+    this.reviewId = reviewId;
+    this.author = author;
+    this.subject = subject;
+    this.content = content;
+  }
 }
