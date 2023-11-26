e45d4676dec01159539b64ffd59a8bb403ae7d82
iannagasen <iann.agasen@gmail.com>
2023-11-26 18:17:40 +0800

    Spring Cloud Gateway Rebaseline

--- spring-cloud/gateway/src/main/java/dev/agasen/microsrv/cloud/gateway/GatewayApplication.java
+++ spring-cloud/gateway/src/main/java/dev/agasen/microsrv/cloud/gateway/GatewayApplication.java
@@ -2,6 +2,9 @@ package dev.agasen.microsrv.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -10,4 +13,13 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+
+	@Bean
+	@LoadBalanced
+	public WebClient.Builder webClientBuilder() { 
+		/**
+		 * This is load balanced which makes it aware of microservice instances registered in the discovery server
+		 */
+		return WebClient.builder();
+	}
 }
